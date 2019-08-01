ac452b4cde9475607a2dd149dc7c70adc45b1fd7
Noreen Wu <noreenwu@gmail.com>
2019-08-01 16:30:59 +0000

    fix: put images into /public/images so they don't have to be imported

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { formatQuestion, formatDate } from '../utils/helpers'
-import FAvatar from '../../images/user-female-100.jpg';
 
 
 class Question extends Component {
@@ -15,23 +14,22 @@ class Question extends Component {
     const {
       name, avatar, timestamp, optionA, optionB
     } = question    
+    
     return(
       <div className="question">
 
       	  <div>
               <div className="question-info">{name} asks...</div>
-			  <img alt={name} 
-				   src={FAvatar}
-				   className='avatar'
-			  />
+
+			  <img src={avatar}
+				   alt={`Avatar of ${name}`} 
+				   className='avatar'/>     
 			   <span>{formatDate(timestamp)}</span>
 		  </div>
 		  <div className='question-text'>
               <span><h3>Would you rather...</h3><p>{optionA}</p><p>OR</p><p>{optionB}</p></span>
 			  <button>View Poll</button>
-			  <img src={avatar}
-				   alt={`Avatar of ${name}`} 
-				   className='avatar'/>             
+        
 		  </div>
       </div>
     )
