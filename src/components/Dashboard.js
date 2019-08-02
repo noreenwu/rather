21a27df90073771012b9d2c53914d9818952a005
Noreen Wu <noreenwu@gmail.com>
2019-08-02 21:28:47 +0000

    feat: made logic change: "unanswered" questions are those not answered by the authedUser, not unanswered by anyone. Also introduced buttons to toggle from user's unanswered questions to user's answered questions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,13 +3,44 @@ import { connect } from 'react-redux'
 import Question from './Question'
 
 class Dashboard extends Component {
-  render() {
 
+  state = {
+    questionIds: this.props.unansweredIds,
+	questionSet: 'unanswered'    // by default display questions the user has not answered
+  } 
+
+  handleChange(val) {
+	if (val === 'answered') {
+      this.setState( {
+          questionIds: this.props.answeredIds
+      })      
+    } else if (val === 'unanswered') {
+      this.setState( {
+          questionIds: this.props.unansweredIds
+      })        
+	}
+  }
+  
+  render() {    
+    
     return (
       <div className="center">
          <h3>Dashboard</h3>
+    
+       	  <button
+      		type='button' 
+      		value='unanswered'
+		    onClick={ () => this.handleChange(event.target.value) }
+      	  >Unanswered</button>
+      
+		  <button
+      		type='button'
+      		value='answered'
+		    onClick={(event) => this.handleChange(event.target.value)}
+      	  >Answered</button>
+      
           <ul className='dashboard-list'>
-            {this.props.questionIds.map((id) => (
+            {this.state.questionIds.map((id) => (
               <li key={id}>
                 <Question id={id}/>
               </li>
@@ -20,15 +51,24 @@ class Dashboard extends Component {
   }
 }
 
-function mapStateToProps( {questions} ) {
+function mapStateToProps( {questions, authedUser}) {
   
   const qvals = Object.values(questions);
-  const answered = qvals.filter(q => q.optionOne.votes.length + q.optionTwo.votes.length === 0);
+  
+  // TODO: change logic so we look for questions that the authed_user has not answered,
+  // instead of questions that no one has answered  
+  const unanswered = qvals.filter(q => !q.optionOne.votes.includes(authedUser) &&
+                                  	   !q.optionTwo.votes.includes(authedUser));
+  
+  const unansweredIds = unanswered.map(q => q.id);
+  
+  const answered = qvals.filter(q => unansweredIds.indexOf(q.id) === -1);
   const answeredIds = answered.map(q => q.id);
   
-  return { 
-    questionIds: answeredIds,
-  }
+  return {
+    	unansweredIds: unansweredIds,
+    	answeredIds: answeredIds
+    }
 }
 
 export default connect(mapStateToProps)(Dashboard)
