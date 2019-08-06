70dcc0ffab1d81bedd06ad5004375d369bf317d5
Noreen Wu <noreenwu@gmail.com>
2019-08-06 19:21:41 +0000

    feat: set up component BarResult to display the results of a specific Question's poll

---
+++ src/components/BarResult.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BarResult = (props) => {
+  let voteSingPlur = 'votes'
+  if (props.votesTotal === 1) {
+    voteSingPlur = 'vote'
+  }
+  return(
+    <div class="bar-result">
+       {props.votesFor} out of {props.votesTotal} {voteSingPlur}
+    </div>
+  )
+}
+
+BarResult.propTypes = {
+  votesFor: PropTypes.number.isRequired,
+  votesTotal: PropTypes.number.isRequired,
+}
+
+export default BarResult
--- src/components/PollResults.js
+++ src/components/PollResults.js
@@ -3,10 +3,11 @@ import { connect } from 'react-redux'
 import { formatQuestion } from '../utils/helpers'
 import Avatar from './Avatar'
 import Option from './Option'
+import BarResult from './BarResult'
 
 class PollResults extends Component {
   render() {
-    const { question } = this.props
+    const { question, votesOne, votesTwo } = this.props
     
     if (question === null) {
       return <p>This Question doesn't exist</p>
@@ -14,12 +15,13 @@ class PollResults extends Component {
     const {
       name, avatar, optionA, optionB
     } = question    
-    
+
+
     return(
        <div className="question-frame">
           <div className="title">Asked by {name}</div>
 	      
-      	  <h3>Poll Results</h3>
+
 		  <div className="question">
               <div className="user-info">				    
 
@@ -27,8 +29,13 @@ class PollResults extends Component {
 
               </div>
               <div className="question-info">
+      	  		  <h3>Results</h3>
    				  <Option option={optionA}/>
+				  <BarResult votesFor={votesOne} votesTotal={votesOne + votesTwo}/>
+
 				  <Option option={optionB}/>
+				  <BarResult votesFor={votesTwo} votesTotal={votesOne + votesTwo}/>
+
 			  </div>
 	      </div>
       
@@ -39,12 +46,15 @@ class PollResults extends Component {
 
 function mapStateToProps({authedUser, users, questions}, {id}) {
   const question = questions[id]
-                          
+  //const votesOne = 0
+  
   return {
     authedUser,
     question: question
     	? formatQuestion(question, users[question.author], authedUser)
-        : null,                  
+        : null,
+    votesOne: question.optionOne.votes.length,
+    votesTwo: question.optionTwo.votes.length
    }
 }
 
