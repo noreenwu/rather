6c0dedf3aa7b1381181e8c25c667b2673e858e33
Noreen Wu <noreenwu@gmail.com>
2019-08-07 04:07:17 +0000

    feat: Ballot displays radio buttons with submit button, and the user's choice is received but not yet recorded permanently via the API

--- src/components/Ballot.js
+++ src/components/Ballot.js
@@ -1,9 +1,32 @@
-import React, { Component } from 'react'
+//import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { formatQuestion } from '../utils/helpers'
 import Avatar from './Avatar'
 
-class Ballot extends Component {
+class Ballot extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }  
+
+  handleChange(event) {
+	console.log("Ballot: handleChange");
+    this.setState( {
+      value: event.target.value
+    })
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();    
+    alert('A choice was submitted: ' + this.state.value);
+
+  }  
+  
+  
   render() {
     const { question } = this.props
     if (question === null) {
@@ -24,9 +47,24 @@ class Ballot extends Component {
               <div className="user-info">
                  <Avatar avatar={avatar} name={name}/>
               </div>
+
  		      <div className="question-info">
-{optionA} or {optionB}
-<button>Submit</button>
+		      <h3>Would you rather...</h3>
+		      <form onSubmit={this.handleSubmit}>
+                  <fieldset>
+                    <div className="choices">
+                       <input type="radio" name="choice" value="optionA" onChange={this.handleChange}/>
+                       <label htmlFor="optionA">{optionA}</label>
+                    </div>
+                    <p>OR</p>
+                    <div className="choices">
+                       <input type="radio" name="choice" value="optionB" onChange={this.handleChange}/>
+                       <label htmlFor="optionB">{optionB}</label>
+                    </div>
+                   </fieldset>
+				  <button>Submit</button>
+			   </form>
+
 			  </div>
           </div>
        </div>
