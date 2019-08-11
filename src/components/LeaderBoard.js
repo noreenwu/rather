fde465df195bc1c8d7b84a6f3c0bdffa00547ae9
Noreen Wu <noreenwu@gmail.com>
2019-08-11 22:40:20 +0000

    feat: built a basic UI for LeaderBoard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import UserStatus from './UserStatus'
 
 class LeaderBoard extends Component {
    render() {
      return(
-       <div>
+       <div className="center">
 	       LeaderBoard
-              <ul className='user-list'>
-                {this.props.userIds.map((id) => (
+              <ul className="user-list">
+                {this.props.sortedIds.map((id) => (
                   <li key={id}>
-					{id}
+					<UserStatus id={id}/>
                   </li>
                 ))}
               </ul>           
@@ -23,19 +24,19 @@ function mapStateToProps( {users} ) {
     // TODO: users need to be sorted in order of highest to lowest score, which is 
     //   answered questions + created questions
   
-    const userIds = Object.keys(users);  
+    // const userIds = Object.keys(users);  
     const userVals = Object.values(users);
-    console.log("Leaderboard: uservals", userVals);
+    // console.log("Leaderboard: uservals", userVals);
     const sorted = userVals.sort((a, b) => (Object.values(a.answers).length + Object.values(a.questions).length > 
                                             Object.values(b.answers).length + Object.values(b.questions).length 
                                  			? -1 : 1))
     console.log("Leaderboard: userVals sorted", sorted);
 	sorted.forEach(function (item) {
-      console.log(item.id);
+      console.log(item.id, item.name);
     })
-                   
+	const sortedIds = sorted.map(x => x.id);                   
     return {
-      userIds
+      sortedIds
     }
 }
 //export default LeaderBoard
