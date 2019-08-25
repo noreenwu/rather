b10366c420e54beb0d04aee9f97908b5bd6c7018
Noreen Wu <noreenwu@gmail.com>
2019-08-25 06:53:49 +0000

    clean: removed some commented out code and deleted index.css (using App.css)

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App';
-import './css/index.css';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
-//import thunk from 'redux-thunk';
 const store = createStore(reducer, middleware)
 
-//const store = createStore(reducer, applyMiddleware(thunk));
 
 
 ReactDOM.render(
