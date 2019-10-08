8c8b4cd8663b30f03ad9011c4260973d84b862f6
Noreen Wu <noreenwu@gmail.com>
2019-10-07 21:08:21 -0400

    build: making changes for the build

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: '/images/user-female-100.jpg',
+    avatarURL: '../images/user-female-100.jpg',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionOne',
