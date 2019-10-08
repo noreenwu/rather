33d30cda35c554d624a464e54abc26e9356d4144
Noreen Wu <noreenwu@gmail.com>
2019-10-07 22:16:12 -0400

    build: changes to package.json to allow build; changes to handling of images to allow hosting at github.io

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Sarah from '../images/user-female-100.jpg'
+import Tyler from '../images/user-male-100.jpg'
+import Penguin from '../images/penguin-100x100.jpg'
+
 
 const Avatar = (props) => {
-  return(
-      <img className="avatar" 
-      alt="avatar of {props.name}"
-      src={props.avatar}/>    
-  )
+    if (props.avatar === 'female') {
+	return (
+	   <img className="avatar" 
+                 alt="avatar of {props.name}"
+                src={Sarah}/>    	    
+	)
+    }
+    else if (props.avatar === 'male') {
+	return (
+	   <img className="avatar" 
+                 alt="avatar of {props.name}"
+                src={Tyler}/>    	    	    
+	)
+    }
+
+    else {
+	return (
+	   <img className="avatar" 
+                 alt="avatar of {props.name}"
+                src={Penguin}/>    	    	    
+	)
+    }
+
 }
 
 Avatar.propTypes = {
--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: '../images/user-female-100.jpg',
+    avatarURL: 'female',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionOne',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: '/images/user-male-100.jpg',
+    avatarURL: 'male',
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: '/images/penguin-100x100.jpg',
+    avatarURL: 'penguin',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
